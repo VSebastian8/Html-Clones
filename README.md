335897da46aed786a5765bd1137bb8d1a52d10bd
Visan Sebastian <emanuel-sebastian.visan-draghicescu@s.unibuc.ro>
2025-03-10 17:42:54 +0200

    Concurrency through Futures

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ This first approached was based on the ideea that clones generally have the same
 
 ## Scalability
 
-In Scala, we can easily improve scalability by using `lazy evaluation` in terms of LazyList. When working large amounts of data, we can avoid unnecessary computation and reduce memory usage.
+In Scala, we can easily improve scalability by using `lazy evaluation` in terms of **LazyList**. When working large amounts of data, we can avoid unnecessary computation and reduce memory usage.
 
 In this case, we would not want to hold a list with all of the parsed html files in memory. We would like to take a single file, process it, free any memory no longer needed, and then finally move on to the next file. By using a lazy list, we can achieve a similar behaviour while still writing the code in a nice, easy to read, `functional` style.
+
+Another idea here is to take advantage of `parallelism`, using multiple threads for processing the list of files in the target directory. Scala offers effortless `concurrency` through **Futures**. The clustering algorithm parses the html and calculates the length of the content in such a list of futures, that are then joined and awaited.
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,61 +2,102 @@ import net.ruippeixotog.scalascraper.browser.{JsoupBrowser}
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL.Parse._
+import net.ruippeixotog.scalascraper.browser.{Browser}
+import net.ruippeixotog.scalascraper.model.Document
 
 import net.ruippeixotog.scalascraper.model._
 
 import os.*
-import net.ruippeixotog.scalascraper.browser.{Browser}
-import net.ruippeixotog.scalascraper.model.Document
+import scala.concurrent.{Await, Future}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.Success
+import scala.util.Failure
+
+// Config variables
+val tier = 1
+val threads = 4
+val alpha = 15
+val max_wait = 60
 
 // Read file names from the specific tier
 // Parse the html from each file
 // Group the sites by the length of their content
 // Write the groupings to the output file
 @main def group_clones(): Unit =
-  val tier = 1
   println("Grouping clones for tier " + tier)
-  val files = os.list(os.pwd / "input" / ("tier" + tier))
-  // Parse the HTML in each file
-  val browser: Browser = JsoupBrowser()
-  val docs = LazyList.from(files)
-      .map(_.toString())
-      .map((s: String) => (s.split("/").last, browser.parseFile(s)))
-  val groups = group_content_lengths(docs, 15)
+  // Read the files
+  val files = read_files(tier)
+  // Clustering algorithm
+  val groups = content_length_clustering(files)
+  // Write to output
   write_groups(os.pwd / "output" / ("tier" + tier + ".txt"), groups)
 
-// Extract the content from each html page
-// Calculate the total length of the content per page
+// Read from the specified subdirectory
+def read_files(tier: Int): IndexedSeq[String] =
+  os.list(os.pwd / "input" / ("tier" + tier)).map(_.toString())
+
+// First algorithm
+def content_length_clustering(
+    files: IndexedSeq[String]
+): List[(Int, List[String])] =
+  // Split the list over n threads
+  val split_files =
+    files.grouped((files.length.toDouble / threads).ceil.toInt).toList
+  assert(
+    split_files.map(_.length).sum == files.length,
+    s"Splitting the list in $threads failed"
+  )
+  // Parse the HTML for each page in each group, then calculate the length of the content
+  val list_futures = split_files.map(group =>
+    Future {
+      // Browser for parsing the HTML
+      val browser: Browser = JsoupBrowser()
+      // Tags that contain relevant content
+      val content_tags =
+        List("p", "a", "h1", "h2", "h3", "h4", "h5", "h6", "li", "span", "div")
+        // Extract the content from each html page
+        // Calculate the total length of the content per page
+      LazyList
+        .from(group)
+        .map((s: String) => (s.split("/").last, browser.parseFile(s)))
+        .map((name, doc) =>
+          (name, content_tags.map(doc >> allText(_)).map(_.length()).sum())
+        )
+    }
+  )
+  // Await the future
+  Await.result(
+    Future
+      .sequence(list_futures)
+      .map((result: List[LazyList[(String, Int)]]) =>
+        // List of tuples (name, length) where name = html page and length = content in page
+        group_by_content(result.flatten.sorted(Ordering.by(_(1))), alpha)
+      ),
+    max_wait.second
+  )
+
 // Group the pages by this length (within a procentage)
-def group_content_lengths(
-    docs: LazyList[(String, Document)],
+def group_by_content(
+    docs_content: List[(String, Int)],
     distance: Int
 ): List[(Int, List[String])] =
-  // Tags that contain relevant content
-  val content_tags =
-    List("p", "a", "h1", "h2", "h3", "h4", "h5", "h6", "li", "span", "div")
-
-  val docs_content_lengths: LazyList[(String, Int)] =
-    docs.map((name, doc) =>
-      (name, content_tags.map(doc >> allText(_)).map(_.length()).sum())
-    )
-
   // list of (content_length, grouped_sites)
   var groups = List[(Int, List[String])]()
-  docs_content_lengths
-    .sorted(Ordering.by(_(1)))
+  docs_content
     .foreach((name: String, length: Int) => {
-      // Either add the page to the beginning of the list or initialize a new group with that page 
+      // Either add the page to the beginning of the list or initialize a new group with that page
       // Since the content list is sorted, the page is either in the correct range for the first group or it starts a new group
       groups = groups match {
-        case (target, group) :: rest if
-          length < target + (target / distance) + 10
-          => (length, name :: group) :: rest 
-        case _ => (length, List(name)) :: groups 
-      }})
+        case (target, group) :: rest
+            if length < target + (target / distance) + 10 =>
+          (length, name :: group) :: rest
+        case _ => (length, List(name)) :: groups
+      }
+    })
   groups
 
-  // Write to the output file
+// Write to the output file
 def write_groups(path: os.Path, groups: List[(Int, List[String])]): Unit =
   os.write.over(path, "")
   groups.foreach((length, group) => {
